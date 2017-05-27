dfa0cdc1a0f17bb85caced3829226b6cee236d8c
Helen Koike <helen.koike@collabora.com>
2017-05-26 23:59:49 -0300

    tmp: fix accounts_utils.py

--- net/google-compute-engine/patches/patch-google__compute__engine_accounts_accounts__utils.py
+++ net/google-compute-engine/patches/patch-google__compute__engine_accounts_accounts__utils.py
@@ -11,39 +11,12 @@
      self.google_users_file = os.path.join(self.google_users_dir, 'google_users')
  
      self._CreateSudoersGroup()
-@@ -71,7 +71,7 @@
-     """Create a Linux group for Google added sudo user accounts."""
-     if not self._GetGroup(self.google_sudoers_group):
-       try:
--        subprocess.check_call(['groupadd', self.google_sudoers_group])
-+        subprocess.check_call(['pw', 'groupadd', self.google_sudoers_group])
-       except subprocess.CalledProcessError as e:
-         self.logger.warning('Could not create the sudoers group. %s.', str(e))
- 
 @@ -126,7 +126,7 @@
      #
      # To solve the issue, make the password '*' which is also recognized
      # as locked but does not prevent SSH login.
 -    command = ['useradd', '-m', '-s', '/bin/bash', '-p', '*', user]
-+    command = ['pw', 'useradd', user, '-m']
++    command = ['useradd', '-m', user]
      try:
        subprocess.check_call(command)
      except subprocess.CalledProcessError as e:
-@@ -148,7 +148,7 @@
-     """
-     groups = ','.join(groups)
-     self.logger.debug('Updating user %s with groups %s.', user, groups)
--    command = ['usermod', '-G', groups, user]
-+    command = ['pw', 'usermod', user, '-G', groups]
-     try:
-       subprocess.check_call(command)
-     except subprocess.CalledProcessError as e:
-@@ -317,7 +317,7 @@
-     """
-     self.logger.info('Removing user %s.', user)
-     if self.remove:
--      command = ['userdel', '-r', user]
-+      command = ['pw', 'userdel', user, '-r']
-       try:
-         subprocess.check_call(command)
-       except subprocess.CalledProcessError as e:
